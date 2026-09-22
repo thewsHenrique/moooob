71d720eaf680ced0867aeac7404027a8fafb5b1a
MatheusHenrique <134979951+thewsHenrique@users.noreply.github.com>
2026-09-22 21:08:30 +0000

    exercicio3 ok

--- app-laboratorio/App.js
+++ app-laboratorio/App.js
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-const nome = "Matheus";
+const eu = {
+  nome: "matheus",
+  idade: "18",
+  cidade: "cascavel"
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{nome}</Text>
+      <Text>{eu.nome}</Text>
+      <text>{eu.idade}</text>
+      <text>{eu.cidade}</text>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
